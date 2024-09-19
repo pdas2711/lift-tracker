755cfaa9187eb557c383da5863ee67fd7f5b7869
Pial Das <pdas2711@proton.me>
2024-09-18 23:39:35 -0400

    Moved adding a movement to its own function, added group field to movement, and added ability to specify group as argument for import_csv.

--- lift-tracker.py
+++ lift-tracker.py
@@ -3,15 +3,19 @@
 import json
 from sys import argv
 
+def add_movement_id(movements, movement, group):
+    movement_entry = {
+                "name": movement,
+                "group": group
+            }
+    movements[str(len(movements) + 1)] = movement_entry
+    return str(len(movements))
+
 def get_movement_id(movements, movement):
     for index in movements:
         if movements[index]["name"] == movement:
             return index
-    movement_entry = {
-                "name": movement
-            }
-    movements[str(len(movements) + 1)] = movement_entry
-    return str(len(movements))
+    return None
 
 def get_location_id(locations, location):
     for index in locations:
@@ -20,7 +24,7 @@ def get_location_id(locations, location):
     locations[str(len(locations) + 1)] = location
     return str(len(locations))
 
-def import_csv(data_file, location, filename):
+def import_csv(data_file, location, group, filename):
     with open(filename, "r") as f:
         csv_data = f.read().splitlines()
     header_data = csv_data.pop(0)
@@ -41,7 +45,10 @@ def import_csv(data_file, location, filename):
                 continue
             else:
                 entry["date"] = date
-                entry["movement"] = get_movement_id(data_file["movements"], column)
+                movement_id = get_movement_id(data_file["movements"], column)
+                if not movement_id:
+                    movement_id = add_movement_id(data_file["movements"], header_data.split(",")[col_index], group)
+                entry["movement"] = movement_id
                 entry["average_metric"] = record.split(",")[col_index]
             data_file["data"].append(entry)
 
@@ -59,18 +66,27 @@ if argv[1] == "new":
     generate_data_file(data_filename)
 elif argv[1] == "import":
     set_location = False
+    set_group = False
     location = None
+    group = None
     for arg in argv:
         if set_location:
             location = arg
             set_location = False
+        if set_group:
+            group = arg
+            set_group = False
         if arg == "--location":
             set_location = True
+        if arg == "--group":
+            set_group = True
     if not location:
         location = input("Location: ")
+    if not group:
+        group = input("Group: ")
     with open(data_filename, "r") as f:
         data_file = json.loads(f.read())
-    import_csv(data_file, location, argv[-1])
+    import_csv(data_file, location, group, argv[-1])
     with open(data_filename, "w") as f:
         f.write(json.dumps(data_file))
 else:
